ac43dfd1c11d71c4b643ee43992ff666196028af
akankshakapse02 <akankshakapse92@gmail.com>
2026-02-26 12:36:27 +0530

    Add files via upload

---
+++ tka/Library.java
@@ -0,0 +1,21 @@
+package com.tka;
+
+public class Library {
+
+	static String libraryName = "Knowledge Hub Library";
+	static long totalBooks = 10000l;
+	static int members;
+	static long availableBooks = 80000l;
+	static double membershipfees;
+	static float fineamout;
+
+	public static void main(String[] args) {
+		System.out.println(libraryName);
+		System.out.println(fineamout);
+		System.out.println(members);
+		System.out.println(availableBooks);
+		System.out.println(membershipfees);
+
+	}
+
+}
---
+++ tka/Student.java
@@ -0,0 +1,23 @@
+package com.tka;
+
+public class Student {
+	static String name = "akanksha";
+	static int age = 22;
+	static long mobileNo;
+	static char email;
+	static double fees;
+	static boolean presenty;
+	static String department = "IT";
+
+	public static void main(String[] args) {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(mobileNo);
+		System.out.println(email);
+		System.out.println(fees);
+		System.out.println(presenty);
+		System.out.println(department);
+
+	}
+
+}
